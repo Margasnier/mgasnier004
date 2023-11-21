07f18eb9098f821a5a86e154a425968b06951c31
DE WOEWODSKY Damien <Damien.De_Woewodsky@bordeaux-inp.fr>
2023-11-21 12:20:28 +0100

    Maj encours_damien

--- encours_damien.c
+++ encours_damien.c
@@ -6,58 +6,39 @@
 
 
 
-/**
- * Fait feu à la position `pos` sur la flotte `fleet` (de `fleet_size`
- * bateaux) et change l'état du bateau s'il est touché.
- *
- *   -1    => Dans l'eau (i.e., pas de bateau à la position `pos`)
- *   0     => Touche le bateau couvrant la position `pos`
- *   X > 0 => Le bateau couvrant la position `pos` est coulé et son type est
- *            X-1 (Exemple 1 => DESTROYER)
- */
 
-int fire(struct rules rules, struct position pos,
-        int fleet_size, struct ship fleet[]) 
+struct position ask_position(pos, )
 {
-  struct ship *tgt = find_target(rules, pos, fleet_size, fleet);
 
-  if (tgt != NULL)
-    {
-    int n = offset(pos, tgt->position);
-    tgt->state[n] = 1;
-    if (remaining_life(rules, *tgt) == 0)
-      return tgt->kind + 1;
-    else
-      return 0;
-  }
-  else 
-    return -1;
+  // demander une position au joueur
+  char pos[10] ;
+  printf("Entrez des coordonnées de bateaux a placer\n");
+  fgets(pos, 10, stdin);
+
+  //transformer la position en (chiffre;chiffre)
+  struct position position = parse_position(pos);
+
+  //contraindre la position dans la grille
+  position = constrain(pos, dim);
+
+  //verifier qu'on se remet pas sur un autre bateau apres le constrain
+  ship_overlap(rules, ship, fleet_size, fleet);
+ 
+  
+
+  return position;
 }
-
 
 
 int main (int argc, char *argv[])
 {
-  struct rules r = {
-    {1, 1, 1, 1, 1},
-    {2, 2, 3, 4, 5}
-  };
+  struct dimension dim = {10, 10}
 
-  struct ship fleet[] = {
-    {CRUISER, {1, 1}, HORIZONTAL, {0, 0, 0, 0, 0}},
-  };
-  
-  int fleet_size = 1;
-    
-  struct position pos = {0,0};
-  
-  int result = fire(r, pos, fleet_size, fleet);
-  printf("fire ! %d\n", result);
+  ask_position(pos, grid);
+  printf("la position est %d %d \n", position.x, position.y);
 
-  pos = {1,0};
   
-  result = fire(r, pos, fleet_size, fleet);
-  printf("fire ! %d\n", result);
 
-  return 0 ;
+  return 0;
+
 }
