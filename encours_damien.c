31ca0294e279947229e9e9d5ea2d9148c159fe28
DE WOEWODSKY Damien <Damien.De_Woewodsky@bordeaux-inp.fr>
2023-11-23 12:11:29 +0100

    Travail du encours_damien.c

--- encours_damien.c
+++ encours_damien.c
@@ -7,37 +7,101 @@
 
 
 
-struct position ask_position(pos, )
+struct position ask_position(struct dimension dim)
 {
 
   // demander une position au joueur
   char pos[10] ;
-  printf("Entrez des coordonnées de bateaux a placer\n");
+  printf("Entrez des coordonnées : \n");
   fgets(pos, 10, stdin);
+   
 
   //transformer la position en (chiffre;chiffre)
   struct position position = parse_position(pos);
 
   //contraindre la position dans la grille
-  position = constrain(pos, dim);
+  struct position poscorrect = position ;
+  constrain( &poscorrect,  dim);
 
-  //verifier qu'on se remet pas sur un autre bateau apres le constrain
-  ship_overlap(rules, ship, fleet_size, fleet);
- 
-  
+  //debugage
+  /* printf("position \%d,%\d \n", position.x, position.y); */
+  /* printf("poscorrect \%d,%\d \n", poscorrect.x, poscorrect.y); */
 
-  return position;
+  //correction de la position si besoin + return de la position
+  if ( poscorrect.x != position.x || poscorrect.y != position.y)
+    {
+      printf("Desolé, ca rentre pas dans la grille, mais j'ai changé ca en \%d,%\d ;) \n", poscorrect.x, poscorrect.y);
+      return poscorrect;
+    }
+  else
+    {
+      return position;
+    }
 }
+
+
+struct ship ask_ship(struct dimension dim)
+{
+
+  //Kind bateau
+  
+// demander une position au joueur
+  char pos[10] ;
+  printf("Entrez des coordonnées : \n");
+  fgets(pos, 10, stdin);
+   
+  //transformer la position en (chiffre;chiffre)
+  struct position position = parse_position(pos);
+
+  //contraindre la position dans la grille
+  struct position poscorrect = position ;
+  constrain( &poscorrect,  dim);
+
+  //correction de la position si besoin 
+  if ( poscorrect.x != position.x || poscorrect.y != position.y)
+    {
+      printf("Desolé, ca rentre pas dans la grille, mais j'ai changé ca en \%d,%\d ;) \n", poscorrect.x, poscorrect.y);
+      return poscorrect;
+    }
+  else
+    {
+      return position;
+    }
+
+
+
+}
+
+
+
+
 
 
 int main (int argc, char *argv[])
 {
-  struct dimension dim = {10, 10}
+  struct dimension dim = {8, 8};
 
-  ask_position(pos, grid);
-  printf("la position est %d %d \n", position.x, position.y);
+
+  for (int i = 0; i < 3; ++i)
+    fleet[i] = ask_ship(dim);
+  
 
   
+  printf("Placement du  croiseur \n");
+  struct position croiseur =  ask_position(dim);
+  printf("la position du croiseur est %d %d \n", croiseur.x, croiseur.y);
+
+  printf("Placement du  sous marin \n");
+  struct position submarine =  ask_position(dim);
+  printf("la position du sous marin est %d %d \n", submarine.x, submarine.y);
+
+  printf("Placement du destroyer \n");
+  struct position destroyer =  ask_position(dim);
+  printf("la position du destroyer est %d %d \n", destroyer.x, destroyer.y);
+
+  struct ship fleet[3] = {
+    {CRUISER, croiseur, HORIZONTAL},
+  };
 
   return 0;
 
